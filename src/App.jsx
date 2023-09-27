8ea292e811ac2964d7f55398c82da2e57c6de920
Rajan mabo limbu <rajanmabolimbu@kcc.edu.np>
2023-09-28 00:00:30 +0545

    New file Button.jsx,OperationButton.jsx created,Modifiaction:App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,59 @@
+import { useReducer } from "react";
+import Button from "./Button";
+import OperationButton from "./OperationButton";
 import "./style.css";
 
+export const ACTIONS = {
+  ADD_DIGIT: "add-digit",
+  DELETE_DIGIT: "delete-digit",
+  CHOOSE_OPERATION: "choose-operation",
+  CLEAR: "clear",
+  EVALUATE: "evaluate",
+};
+
+const reducer = (state, { type, payload }) => {
+  switch (type) {
+    case ACTIONS.ADD_DIGIT:
+      return {
+        ...state,
+        currentOperand: `${state.currentOperand || ""}${payload.digit}`,
+      };
+  }
+};
+
 function App() {
+  const [{ previousOperand, currentOperand, operation }, dispatch] = useReducer(
+    reducer,
+    {}
+  )
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand">123</div>
-        <div className="current-operand">123</div>
+        <div className="previous-operand">{previousOperand}</div>
+        <div className="current-operand">{currentOperand}</div>
       </div>
       <button className="span-two">AC</button>
       <button>DEl</button>
-      <button>﹪</button>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>*</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>+</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button>-</button>
-      <button>.</button>
-      <button>0</button>
+
+      <OperationButton operation="﹪" dispatch={dispatch} />
+      <Button digit="1" dispatch={dispatch} />
+      <Button digit="2" dispatch={dispatch} />
+      <Button digit="3" dispatch={dispatch} />
+      <OperationButton operation="*" dispatch={dispatch} />
+
+      <Button digit="4" dispatch={dispatch} />
+      <Button digit="5" dispatch={dispatch} />
+      <Button digit="6" dispatch={dispatch} />
+      <OperationButton operation="+" dispatch={dispatch} />
+
+      <Button digit="7" dispatch={dispatch} />
+      <Button digit="8" dispatch={dispatch} />
+      <Button digit="9" dispatch={dispatch} />
+      <OperationButton operation="-" dispatch={dispatch} />
+
+      <Button digit="." dispatch={dispatch} />
+      <Button digit="0" dispatch={dispatch} />
+
       <button className="span-two">=</button>
     </div>
   );
---
+++ src/OperationButton.jsx
@@ -0,0 +1,13 @@
+import { ACTIONS } from "./App";
+
+export default function Button(dispatch, operation) {
+  return (
+    <button
+      onClick={() =>
+        dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation } })
+      }
+    >
+      {operation}
+    </button>
+  );
+}
